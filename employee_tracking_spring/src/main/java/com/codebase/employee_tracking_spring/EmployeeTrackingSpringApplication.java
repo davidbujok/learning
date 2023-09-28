089a86563f3dc2f2b0125c489c953537ac01a123
davidbujok <davebujok@gmail.com>
2023-09-28 11:42:30 +0100

    employee spring practice

---
+++ employee_tracking_spring/src/main/java/com/codebase/employee_tracking_spring/EmployeeTrackingSpringApplication.java
@@ -0,0 +1,13 @@
+package com.codebase.employee_tracking_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeTrackingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeTrackingSpringApplication.class, args);
+	}
+
+}
---
+++ employee_tracking_spring/src/test/java/com/codebase/employee_tracking_spring/EmployeeTrackingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codebase.employee_tracking_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeTrackingSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
