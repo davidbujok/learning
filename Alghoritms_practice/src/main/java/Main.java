5fe78c17fb660b75757e6b8aca663d448e7e3ffe
davidbujok <davebujok@gmail.com>
2023-09-28 11:32:33 +0100

    Learning hub

---
+++ Alghoritms_practice/src/main/java/Main.java
@@ -0,0 +1,79 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class Main {
+    public int[] twoSum(int[] nums, int target) {
+        int n=nums.length;
+        Map<Integer,Integer> map=new HashMap<>();
+        int[] result=new int[2];
+        for(int i=0;i<n;i++){
+            if(map.containsKey(target-nums[i])){
+                result[1]=i;
+                result[0]=map.get(target-nums[i]);
+                return result;
+            }
+            map.put(nums[i],i);
+        }
+        return result;
+    }
+
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+
+                return false;
+        }
+        return stack.isEmpty();
+    }
+
+    public int romanToInt(String s) {
+        Map<Character, Integer> m = new HashMap<>();
+
+        m.put('I', 1);
+        m.put('V', 5);
+        m.put('X', 10);
+        m.put('L', 50);
+        m.put('C', 100);
+        m.put('D', 500);
+        m.put('M', 1000);
+
+        int ans = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (i < s.length() - 1 && m.get(s.charAt(i)) < m.get(s.charAt(i + 1))) {
+                ans -= m.get(s.charAt(i));
+            } else {
+                ans += m.get(s.charAt(i));
+            }
+        }
+
+        return ans;
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
